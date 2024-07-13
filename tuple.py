fde951e0de620d37dfcb3eb0bc1d2b611c348a1e
RohitPawar001 <rppawar491@gmail.com>
2024-07-13 20:00:35 +0530

    initial commit

---
+++ tuple.py
@@ -0,0 +1,13 @@
+#tuple in python are same as list except they are immutable
+#tuple can be enclosed in () brackets
+
+string_tuple = ("hi","welcome")
+num_tuple = (1,2,1)
+mix_tuple = ("hi","welcome",1,2)
+print(mix_tuple)
+
+#comman methods in tuple
+mix_tuple.count(1) #it counts the occurance of the element
+
+mix_tuple.index(1) #it returns the index of the given element
+
